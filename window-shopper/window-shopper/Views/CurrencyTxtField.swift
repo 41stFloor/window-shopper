634753a71b60e70169bae05f7ae0ff3c42c1ce7f
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-23 13:15:48 -0400

    Added placeholder and classes.

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Jonah Travis on 9/23/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2474047517)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+                
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
+        }
+        }
+        
+    
+
+}
