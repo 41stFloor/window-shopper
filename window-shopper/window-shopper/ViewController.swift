928aee2e2f7e46a1f9c6e742d340b3c79606927c
Jonah Travis <master@Jonahs-iMac.fios-router.home>
2019-09-23 12:42:37 -0400

    First commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Jonah Travis on 9/23/19.
+//  Copyright © 2019 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
